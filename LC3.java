d2a93aab27a4d68311673b25d779d4e5d3aac383
Chuck Greenman <charlesgreenman@gmail.com>
2026-01-03 16:39:10 -0500

    Define constants and an array to manage holding register values and memory addressable by LC3

---
+++ LC3.java
@@ -0,0 +1,12 @@
+public class LC3 {
+
+    private static final int MAX_ADDRESSABLE_MEMORY = 65536;
+    private static final int REGISTER_COUNT = 10;
+
+    private final short[] memory = new short[MAX_ADDRESSABLE_MEMORY];
+    private final short[] registers = new short[REGISTER_COUNT];
+
+    public static void main(String[] args) {
+        //
+    }
+}
